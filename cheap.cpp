f028b40b38739dbaa0d4d726262702f37ad8d65f
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-24 17:15:02 +0200

    Refractor find_cheapest to use num parameter
    This is much more desirable than having a hardcoded size_t 5 value.
    
    Also added a test checking if count of products returned is not
    hardcoded.

--- cheap.cpp
+++ cheap.cpp
@@ -4,6 +4,7 @@
 #include <iterator>
 #include <vector>
 #include <chrono>
+#include <random>
 
 struct Product final
 {
@@ -16,9 +17,9 @@ struct Product final
 
 std::vector<Product> find_cheapest(const std::vector<Product>& v, size_t num)
 {
-    if (v.size() <= 5){ return std::vector<Product>(v.begin(), v.end()); }
+    if (v.size() <= num){ return std::vector<Product>(v.begin(), v.end()); }
 
-    std::vector<Product> vec(5, Product(0.0) );
+    std::vector<Product> vec(num, Product(0.0) );
     std::partial_sort_copy(
         v.begin(), v.end(),
         vec.begin(), vec.end(),
@@ -40,6 +41,26 @@ TEST(Cheap, Test)
     auto actual = find_cheapest(products, numCheapest);
     std::sort(std::begin(actual), std::end(actual));
 
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(Cheap, RandomProductCount)
+{
+    std::vector products{ Product{0.10}, Product{0.20}, Product{0.50}, Product{0.80}, Product{0.99},
+                          Product{1.00}, Product{1.20}, Product{1.50}, Product{1.80}, Product{1.99},
+                          Product{2.00}, Product{2.20}, Product{2.50}, Product{2.80}, Product{2.99} };
+
+    std::mt19937 mt{std::random_device{}()};
+    std::uniform_int_distribution a{1,10};
+    auto const numCheapest = a(mt);
+    std::vector<Product> expected;
+    expected.reserve(numCheapest);
+    std::copy(std::begin(products), std::begin(products) + numCheapest, std::back_inserter(expected));
+    std::random_shuffle(products.begin(), products.end());
+    auto actual = find_cheapest(products, numCheapest);
+    std::sort(std::begin(actual), std::end(actual));
+
+    ASSERT_EQ(expected.size(), actual.size());
     ASSERT_EQ(expected, actual);
 }
 
